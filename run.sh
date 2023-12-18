23bf2601415d6588c8453cc949ac31395114a42d
ZiyangJiao <ziyangjiao1998@gmail.com>
2023-12-18 01:22:16 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ sudo rm -rf *.o
 sudo rm -rf *.ko
 sudo rm -rf .*.cmd
 
-sudo make CONFIG_F2FS_FS=m -C/lib/modules/5.15.0-76-generic/build M=/home/femu/linux-5.15.0-76-generic-f2fs/ modules
+sudo make CONFIG_F2FS_FS=m -C/lib/modules/5.15.0-76-generic/build M=/home/femu/FAST24_CVSS_CVFS/ modules
 sudo umount /dev/nvme0n1
 sudo rmmod f2fs
-sudo modprobe zstd_compress && sudo modprobe lz4hc_compress && sudo modprobe lz4_compress && sudo insmod /home/femu/linux-5.15.0-76-generic-f2fs/f2fs.ko
+sudo modprobe zstd_compress && sudo modprobe lz4hc_compress && sudo modprobe lz4_compress && sudo insmod /home/femu/FAST24_CVSS_CVFS/f2fs.ko
