1f84ec7b35aca080edaad815107463ad58d09236
ZiyangJiao <ziyangjiao1998@gmail.com>
2023-12-18 19:16:50 -0500

    Update run.sh

--- README.md
+++ README.md
@@ -8,4 +8,3 @@
  ```bash
  sudo ./run.sh
  ```
- **Note that** it is important to update the path (i.e., -C/lib/modules/5.15.0-76-generic/build M=/home/femu/FAST24_CVSS_CVFS/ modules) in the script using the **aboslute path** before compilation.
--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ sudo rm -rf *.o
 sudo rm -rf *.ko
 sudo rm -rf .*.cmd
 
-sudo make CONFIG_F2FS_FS=m -C/lib/modules/5.15.0-76-generic/build M=/home/femu/FAST24_CVSS_CVFS/ modules
+#sudo make CONFIG_F2FS_FS=m -C/lib/modules/5.15.0-76-generic/build M=/home/femu/FAST24_CVSS_CVFS/ modules
+sudo make -j$(nproc) CONFIG_F2FS_FS=m -C/lib/modules/$(uname -r)/build M=$(pwd) modules
 sudo umount /dev/nvme0n1
 sudo rmmod f2fs
 sudo modprobe zstd_compress && sudo modprobe lz4hc_compress && sudo modprobe lz4_compress && sudo insmod /home/femu/FAST24_CVSS_CVFS/f2fs.ko
